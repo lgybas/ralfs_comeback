385d0bdba1b21ba32a267d163c9d191532387fd2
Liane Gybas <liane.gybas@gmail.com>
2024-11-22 09:13:57 +0100

    update logic implemented

--- main.py
+++ main.py
@@ -22,31 +22,41 @@ from datetime import datetime, timedelta
 import pandas as pd
 
 # Titel der App
-st.title("Countdown-Kalender")
+st.title("Ralfs Comeback")
 
 # Benutzer wählt ein Ziel-Datum
-target_date = st.date_input("Wähle das Ziel-Datum:", value=datetime.now().date())
-
-# Aktuelles Datum und Zeit
-current_time = datetime.now()
+target_date = datetime.strptime("18.12.2025", "%d.%m.%Y").date()
+#st.date_input("Wähle das Ziel-Datum:", value=datetime.now().date())
 
 # Zielzeit auf Mitternacht setzen
 target_datetime = datetime.combine(target_date, datetime.min.time())
 
-# Berechnung der verbleibenden Zeit
-remaining_time = target_datetime - current_time
+# Aktuelles Datum und Zeit
+while remaining_time.total_seconds() > 0:
+    current_time = datetime.now()
+    # Berechnung der verbleibenden Zeit
+    remaining_time = target_datetime - current_time
+    # Zeitanzeige
 
-# Zeitanzeige
-if remaining_time.total_seconds() > 0:
     st.header("Verbleibende Zeit:")
     days = remaining_time.days
     hours, remainder = divmod(remaining_time.seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
     st.subheader(f"{days} Tage, {hours} Stunden, {minutes} Minuten, {seconds} Sekunden")
-else:
-    st.subheader("Das Ziel-Datum ist erreicht! 🎉")
+        
+    time.sleep(1)
 
-# Optional: Benutzerdefinierte Nachricht
-message = st.text_input("Gib eine Nachricht ein, die bei Ablauf des Countdowns angezeigt werden soll:", "🎉 Countdown abgeschlossen!")
-if remaining_time.total_seconds() <= 0:
-    st.write(message)
+st.subheader("Das war's! 🎉")
+
+
+
+
+
+
+
+
+
+# # Optional: Benutzerdefinierte Nachricht
+# message = st.text_input("Gib eine Nachricht ein, die bei Ablauf des Countdowns angezeigt werden soll:", "🎉 Countdown abgeschlossen!")
+# if remaining_time.total_seconds() <= 0:
+#     st.write(message)
